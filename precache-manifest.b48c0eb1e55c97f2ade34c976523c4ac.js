d23419c493dfb26bb659475b847a3605fda36ca2
sachin jadhav <95169535+sachinjadhav633@users.noreply.github.com>
2022-04-05 18:37:31 +0530

    Updates

---
+++ precache-manifest.b48c0eb1e55c97f2ade34c976523c4ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "849f55155b1d50b253af97080b81ad75",
+    "url": "/Todo-App/index.html"
+  },
+  {
+    "revision": "71f15a5028cbb4b9ff34",
+    "url": "/Todo-App/static/css/main.98f6afad.chunk.css"
+  },
+  {
+    "revision": "8e49ff261b0b40148c6b",
+    "url": "/Todo-App/static/js/2.c6b53d6d.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Todo-App/static/js/2.c6b53d6d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71f15a5028cbb4b9ff34",
+    "url": "/Todo-App/static/js/main.9f8b91ca.chunk.js"
+  },
+  {
+    "revision": "9613a20845ef48f3078b",
+    "url": "/Todo-App/static/js/runtime-main.ee7eeae4.js"
+  }
+]);
